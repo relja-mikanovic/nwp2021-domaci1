cb585be8a9730ef7799d5740d96fade013500cc6
relja-mikanovic <rmikanovic6719rn@raf.rs>
2021-10-31 19:44:57 +0100

    -Zapoceta postavka -Napravljene stranice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TokenConfigurationComponent } from './components/token-configuration/token-configuration.component';
+import { EntityExtractionComponent} from "./components/entity-extraction/entity-extraction.component";
+import {TextSimilarityComponent} from "./components/text-similarity/text-similarity.component";
+import {LanguageDetectionComponent} from "./components/language-detection/language-detection.component";
+import {SentimentAnalysisComponent} from "./components/sentiment-analysis/sentiment-analysis.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "token",
+    component: TokenConfigurationComponent
+  },
+  {
+    path: "extraction",
+    component: EntityExtractionComponent
+  },
+  {
+    path: "similarity",
+    component: TextSimilarityComponent
+  },
+  {
+    path: "detection",
+    component: LanguageDetectionComponent
+  },
+  {
+    path: "sentiment",
+    component: SentimentAnalysisComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { TokenConfigurationComponent } from './components/token-configuration/token-configuration.component';
+import { EntityExtractionComponent } from "./components/entity-extraction/entity-extraction.component";
+import { TextSimilarityComponent} from "./components/text-similarity/text-similarity.component";
+import { LanguageDetectionComponent } from "./components/language-detection/language-detection.component";
+import { SentimentAnalysisComponent} from "./components/sentiment-analysis/sentiment-analysis.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TokenConfigurationComponent,
+    EntityExtractionComponent,
+    TextSimilarityComponent,
+    LanguageDetectionComponent,
+    SentimentAnalysisComponent
   ],
   imports: [
     BrowserModule,
